577608087814dec6437dad25e316b60f77d8f1c7
Joseph Monticelli <jdmonticelli@gmail.com>
2024-10-05 03:05:11 -0400

    setup chatter script in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { ChatEngine } from "react-chat-engine";
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Chatter Beta</h1>
+     <ChatEngine
+     height="100vh"
+     projectID="db057f23-dcaf-4482-95d9-5392b6a1d9ec"
+     userName="user1"
+     userSecret="123"
+     />
     </div>
   );
 }
